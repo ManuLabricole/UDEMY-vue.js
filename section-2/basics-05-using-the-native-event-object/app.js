370043f0618f5346901a51b3a7973a7297b03845
Manu Labricole <manu_barriol@hotmail.fr>
2023-05-18 13:38:32 +0200

    use computed props

--- section-2/basics-05-using-the-native-event-object/app.js
+++ section-2/basics-05-using-the-native-event-object/app.js
@@ -1,10 +1,26 @@
 const app = Vue.createApp({
+  // Data is a function that returns an object.
+  // The object contains the data that will be used in the template.
   data() {
     return {
       counter: 0,
       name: ''
     };
   },
+
+  computed: {
+    fullName() {
+      console.log('Running again...');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Santos';
+    }
+  },
+  // Methods are functions that are executed when an event is triggered.
+  // The event is passed as an argument to the method.
+  // The event object is a native JavaScript object.
+  // The event object has a target property that contains the element that triggered the event.
   methods: {
     setName(event) {
       this.name = event.target.value;
