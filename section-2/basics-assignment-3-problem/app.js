fe8811887185ac92a47d5f3a4446a7b3ede416fa
Manu Labricole <manu_barriol@hotmail.fr>
2023-05-18 16:27:09 +0200

    start assignement 3

---
+++ section-2/basics-assignment-3-problem/app.js
@@ -0,0 +1,48 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            number: 0,
+        };
+    },
+
+    watch: {
+        resultDisplayed() {
+            console.log('watcher');
+            const that = this;
+            setTimeout(function () {
+                that.number = 0;
+            }, 5000);
+        }
+    },
+
+    computed: {
+        resultDisplayed() {
+            if (this.number < 37) {
+                return 'Not there yet';
+            } else if (this.number > 37) {
+                return 'Too much!';
+            } else {
+                return this.number;
+            }
+
+        }
+
+    },
+
+    methods: {
+        addFive() {
+            console.log('addFive');
+            this.number = this.number + 5;
+            console.log(this.number);
+            
+        },
+        addOne() {
+            console.log('addOne');
+            this.number = this.number + 1;
+            console.log(this.number);
+            
+        },
+    }
+});
+
+app.mount('#assignment');
