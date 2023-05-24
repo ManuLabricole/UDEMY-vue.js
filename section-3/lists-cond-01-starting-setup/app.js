d2361300a852276dc08c462369a8b1a8d017fd6c
Manu Labricole <manu_barriol@hotmail.fr>
2023-05-24 22:58:32 +0200

    Finish course section3

--- section-3/lists-cond-01-starting-setup/app.js
+++ section-3/lists-cond-01-starting-setup/app.js
@@ -1,7 +1,24 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      goals: [],
+      enteredValue: '',
+      display: true
+    };
   },
+  methods: {
+    addGoal() {
+      // We can use push to add an item to an array
+      this.goals.push(this.enteredValue);
+    },
+    removeGoal(idx) {
+      // We can use splice to remove an item from an array
+      this.goals.splice(idx, 1);
+    },
+    toggleDisplay() {
+      this.display = !this.display;
+    }
+  }
 });
 
 app.mount('#user-goals');
