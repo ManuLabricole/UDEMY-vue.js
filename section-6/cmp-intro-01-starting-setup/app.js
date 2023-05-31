af61c8068df196628763d21b5daf1fc4f8e4147a
Manu Labricole <manu_barriol@hotmail.fr>
2023-05-31 22:37:58 +0200

    problem v-for with button

---
+++ section-6/cmp-intro-01-starting-setup/app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            friends: [{
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '01234 5678 991',
+                email: 'manuel_la_tlouelle@gmail.com'
+            }, {
+                id: 'julie',
+                name: 'Julie Jones',
+                phone: '09876 543 221',
+                email: 'julie_gaillet@gmail.com'
+            }
+            ]
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+});
