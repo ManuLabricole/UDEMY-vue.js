f5a994b3918f043e74af95dc4fcbab569b266b7f
Manu Labricole <manu_barriol@hotmail.fr>
2023-06-20 23:45:37 +0200

    add app component friend contact

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-    <div>
         <h2>My friends</h2>
         <ul>
-            <li></li>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
         </ul>
-    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            friends : [
+            friends: [
                 {
-                    id : 'manuel',
-                    name : 'Manuel LaTlouelle',
-                    phone : '0566272836',
-                    email : 'manueél_latlouelle@gmail.com'
+                    id: 'manuel',
+                    name: 'Manuel LaTlouelle',
+                    phone: '0566272836',
+                    email: 'manueél_latlouelle@gmail.com'
                 },
                 {
-                    id : 'manuel',
-                    name : 'Manuel LaTlouelle',
-                    phone : '0566272836',
-                    email : 'manueél_latlouelle@gmail.com'
+                    id: 'manuel',
+                    name: 'Manuel LaTlouelle',
+                    phone: '0566272836',
+                    email: 'manueél_latlouelle@gmail.com'
                 }
             ]
         }
