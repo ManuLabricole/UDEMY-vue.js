edf7eec3ef0fdd2c5546ace949ae4a58c4ffcec6
Manu Labricole <manu_barriol@hotmail.fr>
2023-06-29 00:48:25 +0200

    add comp and style

---
+++ section-10-projet/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <stored-resources :stored-resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from "./components/learning-resources/StoredResources.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    StoredResources,
+  },
+
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 1,
+          title: "Officiel Doc",
+          description: "Vue.js - The Progressive JavaScript Framework",
+          link: "https://vuejs.org/",
+          icon: "school",
+        },
+        {
+          id: 2,
+          title: "Google",
+          description: "Google",
+          link: "https://www.google.com/",
+          icon: "book",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ section-10-projet/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ul>
+            <learning-resource v-for="resource in storedResources" :key="resource.id" :resource="resource"></learning-resource>
+        </ul>
+    </div>
+</template>
+
+<script>
+import LearningResource from "./LearningResource.vue";
+export default {
+    name: "StoredResources",
+    components: {
+        LearningResource,
+    },
+    props: ['storedResources'],
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
