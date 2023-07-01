84f5ebcbacff518dfc1c67abf8ed5e0bb2c18575
Manu Labricole <manu_barriol@hotmail.fr>
2023-07-01 15:28:50 +0200

    start header component

--- section-10-projet/src/components/learning-resources/LearningResource.vue
+++ section-10-projet/src/components/learning-resources/LearningResource.vue
@@ -1,19 +1,20 @@
 <template>
   <li>
-    <div>
+    <base-card>
       <header>
         <h3>{{ resource.title }}</h3>
         <button>Delete</button>
       </header>
-    </div>
-    <p>{{ resource.description }}</p>
-    <nav>
-      <a :href="resource.link">{{ resource.link }}</a>
-    </nav>
+      <p>{{ resource.description }}</p>
+      <nav>
+        <a :href="resource.link">{{ resource.link }}</a>
+      </nav>
+    </base-card>
   </li>
 </template>
 
 <script>
+
 export default {
   props: ['resource'],
 };
