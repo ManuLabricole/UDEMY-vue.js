82d3247e209601f59c2488498ee80815b1d3a810
Manu Labricole <manu_barriol@hotmail.fr>
2023-07-02 22:29:02 +0200

    handle programtically routing

--- section-13-routing/src/main.js
+++ section-13-routing/src/main.js
@@ -5,21 +5,18 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
 
   history: createWebHistory(),
   routes: [
-    {
-      path: '/teams',
-      component: TeamsList,
-    },
-    {
-      path: '/users',
-      component: UsersList,
-    },
-
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+    // What if we want to pass dynamic data to the route?
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
   ],
+  linkActiveClass: 'active',
 });
 
 const app = createApp(App);
